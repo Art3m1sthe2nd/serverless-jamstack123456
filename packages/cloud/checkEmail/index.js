1f7f013751eb3fab32e72d51472f96eb2d6ebb8c
Art3m1sthe2nd <112702174+Art3m1sthe2nd@users.noreply.github.com>
2023-07-20 14:31:18 +0200

    rosponse of postEmail doesnt work

--- packages/cloud/checkEmail/index.js
+++ packages/cloud/checkEmail/index.js
@@ -4,29 +4,28 @@ async function main(args) {
     const uri = process.env['DATABASE_URL'];
     let client = new MongoClient(uri);
 
-    let newEmail = args.email;
+    const newName = args.name; // Access the 'name' property from args
+    const newContactEmail = args.email; // Access the 'email' property from args
+    const newAbteilung = args.Abteilung; // Access the 'Abteilung' property from args
+    const newAnliegen = args.anliegen; // Access the 'anliegen' property from args
+
     try {
         await client.connect();
-        
-        // Check if the email already exists in the database
-        if (existingEmail) {
-            console.log(`${newEmail} already exists in the database. Skipping insertion.`);
-            return { message: `${newEmail} already exists in the database. Skipping insertion.`, inserted: false };
-        }
-        
+
         // If the email doesn't exist, insert it into the database
         await client.db("do-coffee").collection("contact-forms").insertOne({
             name: newName,
             contactEmail: newContactEmail,
             abteilung: newAbteilung,
             anliegen: newAnliegen
-          });
-        console.log(`Added ${newEmail} to the database.`);
-        return { message: `Added ${newEmail} to the database.`, inserted: true };
+        });
+        
+        console.log(`Added ${newName} to the database.`);
+        return { message: `Added ${newName} to the database.`, inserted: true };
     } catch (e) {
         console.error(e);
         return {
-            "body": { "error": "There was a problem adding the email address to the database." },
+            "body": { "error": "There was a problem adding the entry to the database." },
             "statusCode": 400
         };
     } finally {
